1128ace9acdbc4812eee652419194eca3ef11f25
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-22 07:34:00 +0200

    created components

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { defaults as Footer } from './Footer';
+export { defaults as Cart } from './Cart';
+export { defaults as FooterBanner } from './FooterBanner';
+export { defaults as Navbar } from './Navbar';
+export { defaults as Product } from './Product';
+export { defaults as HeroBanner } from './HeroBanner';
+export { defaults as Layout } from './Layout';
