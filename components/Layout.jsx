dc8cb85e1145e8b10d5344a42edf8090c739562d
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-22 22:28:56 +0200

    new bug, doen for tonight..have fun tomorrow

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+// like <head> in html, meta data, need to be imported in Next.js
+import Head from "next/head";
 
-const Layout = () => {
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+// accessing other components via children prop
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="layout">
+      <Head>
+        <title>Ecommerce pratice</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
