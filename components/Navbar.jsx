ffb81ceaa8ade03157955d78ffeab4202b00f052
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-26 18:41:14 +0200

    cart import works

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
 
-import { Cart } from "./Cart";
-import { useStateContext } from "../context/StateContext";
+import Cart from "./Cart";
+// import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
-  const { showCart, setShowCart, totalQuantities } = useStateContext();
+  // const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
     <div className="navbar-container">
@@ -15,7 +15,7 @@ const Navbar = () => {
       </p>
       <button type="button" className="cart-icon" onClick="">
         <AiOutlineShopping />
-        <span className="cart-item-qty">{totalQuantities}</span>
+        <span className="cart-item-qty">0</span>
       </button>
 
       <Cart />
