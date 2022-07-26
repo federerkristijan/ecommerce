62197f1af5c42ab4952bc5338e60df76e226a65d
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-26 16:13:41 +0200

    bug with imports

--- context/StateContext.js
+++ context/StateContext.js
@@ -62,6 +62,7 @@ export const StateContext = ({ children }) => {
     <Context.Provider
       value={{
         showCart,
+        setShowCart,
         cartItems,
         totalPrice,
         totalQuantities,
