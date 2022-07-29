0a44358ad80a922fc43833b0d878dbdf30864eee
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-29 11:36:24 +0200

    cart.jsx in progress

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -12,17 +12,36 @@ import { urlFor } from "../lib/client";
 
 const Cart = () => {
   const cartRef = useRef();
-  const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } =
+    useStateContext();
 
-  return <div className="cart-wrapper" ref={cartRef}>
-    <div className="cart-container">
-      <button type="button" className="cart-heading" onClick={() => setShowCart(false)}>
-        <AiOutlineLeft />
-        <span className="heading">Your Cart</span>
-        <span className="cart-num-items">({totalQuantities} items)</span>
-      </button>
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping is empty</h3>
+            <Link href="/">
+              <button type="button" onClick={() => setShowCart(false)} className="btn" >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
+  );
 };
 
 export default Cart;
