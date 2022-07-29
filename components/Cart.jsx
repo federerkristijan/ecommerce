d42558aff2424ad376d7a04f9133cf2de8074248
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-29 12:39:02 +0200

    cart card done, need qty updates

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -33,10 +33,66 @@ const Cart = () => {
             <AiOutlineShopping size={150} />
             <h3>Your shopping is empty</h3>
             <Link href="/">
-              <button type="button" onClick={() => setShowCart(false)} className="btn" >
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
                 Continue Shopping
               </button>
             </Link>
+          </div>
+        )}
+
+        <div className="product-container">
+          {/* always use custom indexes!  */}
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>{item.price}€</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span className="minus" onClick="">
+                          <AiOutlineMinus />
+                        </span>
+                        <span className="num" onClick="">
+                          0
+                        </span>
+                        <span className="plus" onClick="">
+                          <AiOutlinePlus />
+                        </span>
+                      </p>
+                    </div>
+                    <botton
+                    type="button"
+                    className="remove-item"
+                    onClick=""
+                    >
+                      <TiDeleteOutline />
+                    </botton>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal:</h3>
+              <h3>{totalPrice}€</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className="btn" onClick="" >Pay with Stripe</button>
+            </div>
           </div>
         )}
       </div>
