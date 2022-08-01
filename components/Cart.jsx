c7362d5fd22b697a9fa5f9d44f3be7b45e4bba22
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-01 19:21:52 +0200

    smaller bugs found and fixed

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -12,8 +12,14 @@ import { urlFor } from "../lib/client";
 
 const Cart = () => {
   const cartRef = useRef();
-  const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } =
-    useStateContext();
+  const {
+    totalPrice,
+    totalQuantities,
+    cartItems,
+    setShowCart,
+    toggleCartItemQuantity,
+    onRemove,
+  } = useStateContext();
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -61,21 +67,31 @@ const Cart = () => {
                   <div className="flex bottom">
                     <div>
                       <p className="quantity-desc">
-                        <span className="minus" onClick={() => toggleCartItemQuantity(item._id, 'dec')}>
+                        <span
+                          className="minus"
+                          onClick={() =>
+                            toggleCartItemQuantity(item._id, "dec")
+                          }
+                        >
                           <AiOutlineMinus />
                         </span>
                         <span className="num" onClick="">
                           {item.quantity}
                         </span>
-                        <span className="plus" onClick={() => toggleCartItemQuantity(item._id, 'inc')}>
+                        <span
+                          className="plus"
+                          onClick={() =>
+                            toggleCartItemQuantity(item._id, "inc")
+                          }
+                        >
                           <AiOutlinePlus />
                         </span>
                       </p>
                     </div>
                     <botton
-                    type="button"
-                    className="remove-item"
-                    onClick={() => onRemove(item)}
+                      type="button"
+                      className="remove-item"
+                      onClick={() => onRemove(item)}
                     >
                       <TiDeleteOutline />
                     </botton>
@@ -91,7 +107,9 @@ const Cart = () => {
               <h3>{totalPrice}€</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick="">Pay with Stripe</button>
+              <button type="button" className="btn" onClick="">
+                Pay with Stripe
+              </button>
             </div>
           </div>
         )}
