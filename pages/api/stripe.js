c342db07a65d789ee2443f72528f3c8d09300607
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-02 11:28:18 +0200

    fixed some bugs, added import in stripe.js

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,4 +1,6 @@
-const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
