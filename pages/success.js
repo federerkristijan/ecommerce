4c45b7726b9ac49bd0250ccf807b5dee5a6e5531
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-06 00:44:36 +0200

    success.js done

--- pages/success.js
+++ pages/success.js
@@ -1,16 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import Link from 'next/link'
-import { BsBagCheckFill } from 'react-icons/bs'
-import { useRouter } from 'next/router'
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
 
-import { useStateContext } from '../context/StateContext'
+import { useStateContext } from "../context/StateContext";
 
 const Success = () => {
-  return (
-    <div>
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
 
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+          <h2>Thank you for your order!</h2>
+          <p className="email-msg">Check your email inbox for the invoice.</p>
+          <p className="description">
+            If you have any question{" "}
+            <a className="email" href="mailto:federer.kristijan@gmail.com">federer.kristijan@gmail.com</a>
+          </p>
+          <Link href="/">
+            <button type="button" width="300px" className="btn">Continue shopping</button>
+          </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Success;
