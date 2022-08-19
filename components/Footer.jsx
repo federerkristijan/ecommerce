4c5a384a24e88032f57d68f195f9dc00609c1647
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-19 20:12:35 +0200

    footer updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <p>2022 Ecommerce practice Web App</p>
+      <p>YouTube Tutorial by: JavaScript Mastery</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
